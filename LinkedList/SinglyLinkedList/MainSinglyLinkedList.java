222a6c7ddd2ccebe5d2a373a04c5c6fd4b0b4232
Uchenna Ofodile GitHub <uchenna.ofodile@gmail.com>
2022-11-10 20:46:03 -0500

    Added SinglyLinkedList files

---
+++ LinkedList/SinglyLinkedList/MainSinglyLinkedList.java
@@ -0,0 +1,23 @@
+public class Main {
+
+  public static void main(String[] args) {
+    SinglyLinkedList sLL = new SinglyLinkedList();
+    sLL.createSinglyLinkedList(5);
+    //System.out.println(sLL.head.value);
+    System.out.println("-----Insertion Method-----");
+    sLL.insertInLinkedList(6, 1);
+    sLL.insertInLinkedList(7, 3);
+    sLL.insertInLinkedList(8, 4);
+    sLL.insertInLinkedList(9, 0);
+    //System.out.println(sLL.head.next.next.value); //shows 3rd element in sLL
+    // sLL.traverseSinglyLinkedList();
+    //sLL.searchNode(15);
+    //sLL.deletionOfNode(20);
+    sLL.traverseSinglyLinkedList();
+    System.out.println("The tail is: " + sLL.tail.value);
+
+    System.out.println("Deleting SLL");
+    sLL.deleteSLL();
+    sLL.traverseSinglyLinkedList();
+  }
+}
---
+++ LinkedList/SinglyLinkedList/Node.java
@@ -0,0 +1,7 @@
+public class Node {
+  //Node value
+  public int value;
+  //Reference to the location in
+  //memory of the next node in the list
+  public Node next;
+}
