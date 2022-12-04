bda6ac978fc427c1e8f5eb317a156965b89baae0
Uchenna <uchenna.ofodile@gmail.com>
2022-12-03 21:31:23 -0500

    Added Tree DS

---
+++ Tree/Main.java
@@ -0,0 +1,20 @@
+class Main{
+    public static void main(String[] args){
+        TreeNode drinks = new TreeNode("Drinks"); //Root
+        TreeNode hot = new TreeNode("Hot"); //Child to root
+        TreeNode cold = new TreeNode("Cold");
+        TreeNode tea = new TreeNode("Tea");
+        TreeNode coffee = new TreeNode("Coffee");
+        TreeNode wine = new TreeNode("Wine");
+        TreeNode beer = new TreeNode("Beer");
+        drinks.addChild(hot);
+        drinks.addChild(cold);
+        hot.addChild(tea);
+        hot.addChild(coffee);
+        cold.addChild(wine);
+        cold.addChild(beer);
+        System.out.println();
+        System.out.println(drinks.print(0));
+        
+    }
+}
---
+++ Tree/TreeNode.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class TreeNode{
+    String data;
+    ArrayList<TreeNode> children;
+
+    public TreeNode(String data){
+        this.data = data;
+        this.children = new ArrayList<TreeNode>();
+    }
+
+    public void addChild(TreeNode node){
+        this.children.add(node);
+    }
+
+    public String print(int level){
+        String ret;//return
+        ret = "  ".repeat(level) + data +"\n";//repears string
+        for(TreeNode node : this.children){
+            ret += node.print(level+1);
+        }
+        return ret; 
+    }
+}
