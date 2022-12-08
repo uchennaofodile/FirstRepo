45e560859beb872910fdf2df9a2e439c517a4f8c
Uchenna Ofodile GitHub <uchenna.ofodile@gmail.com>
2022-12-07 23:20:11 -0500

    Added binary tree data

---
+++ Tree/BinaryNode.java
@@ -0,0 +1,9 @@
+public class BinaryNode{
+    public String value;//drinks menu
+    public BinaryNode left;
+    public BinaryNode right;
+    public int height;
+
+
+
+}
---
+++ Tree/BinaryTreeLL.java
@@ -0,0 +1,8 @@
+public class BinaryTreeLL {
+  BinaryNode root;
+
+  //O(1) Space and Time
+  public BinaryTreeLL() {
+    this.root = null; //creating BinaryTreeLL
+  }
+}
