843ae586ccdfaac6ebd75d632cf168586afebfc2
Uchenna Ofodile GitHub <uchenna.ofodile@gmail.com>
2022-12-20 21:20:49 -0500

    Added inorder and postorder traversal

--- Tree/BinaryTreeArray/BinaryTreeArray.java
+++ Tree/BinaryTreeArray/BinaryTreeArray.java
@@ -3,7 +3,7 @@ public class BinaryTreeArray {
   int lastUsedIndex;
 
   public BinaryTreeArray(int size) {
-    arr = new String[size];
+    arr = new String[size + 1];
     this.lastUsedIndex = 0;
     System.out.println(
       "Blank Array Binary Tree of size " + size + " has been created"
@@ -29,5 +29,43 @@ public class BinaryTreeArray {
     } else {
       System.out.println("The BT is full");
     }
+  }
+
+  //PreOrder Traversal
+  //O(N) time and space(inserts elements into the stack memory)
+  public void preOrder(int index) {
+    if (index > lastUsedIndex) {
+      return;
+    }
+    System.out.print(arr[index] + " ");
+    preOrder(index * 2); //O(N/2)
+    preOrder(index * 2 + 1); //O(N/2)
+  }
+
+  //InOrder Traversal
+  //time and space is O(n)
+  public void inOrder(int index) {
+    // base case
+    if (index > lastUsedIndex) {
+      return;
+    }
+    //recursion
+    inOrder(index * 2); //O(n/2)
+    System.out.print(arr[index] + " ");
+    inOrder(index * 2 + 1); //O(n/2)
+  }
+
+  //PostOrder Traversal
+  //Time and space is O(n)
+  public void postOrder(int index) {
+    //base case
+    if (index > lastUsedIndex) {
+      return;
+    }
+
+    //recursion
+    postOrder(index * 2); //O(n/2)
+    postOrder(index * 2 + 1); //O(n/2)
+    System.out.print(arr[index] + " ");
   }
 }
--- Tree/BinaryTreeArray/Main.java
+++ Tree/BinaryTreeArray/Main.java
@@ -1,13 +1,24 @@
+import java.util.Arrays;
+
 class Main {
 
   public static void main(String[] args) {
     System.out.println();
-    BinaryTreeArray binaryTreeArray = new BinaryTreeArray(5);
+    BinaryTreeArray binaryTreeArray = new BinaryTreeArray(9);
+    //Arrays.toString(binaryTreeArray);
     binaryTreeArray.insert("N1");
     binaryTreeArray.insert("N2");
     binaryTreeArray.insert("N3");
     binaryTreeArray.insert("N4");
     binaryTreeArray.insert("N5");
     binaryTreeArray.insert("N6");
+    binaryTreeArray.insert("N7");
+    binaryTreeArray.insert("N8");
+    binaryTreeArray.insert("N9");
+    //binaryTreeArray.preOrder(1);
+    //binaryTreeArray.inOrder(1);
+    System.out.println();
+    //System.out.println(binaryTreeArray.arr[10]);
+    binaryTreeArray.postOrder(1);
   }
 }
